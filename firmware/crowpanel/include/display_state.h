b526f470440b70cd616d76dd3a74abedc101e485
danikeuc <43805752+danikeuc@users.noreply.github.com>
2026-09-21 19:49:06 +0200

    debug: show CrowPanel connectivity cause on display

--- firmware/crowpanel/include/display_state.h
+++ firmware/crowpanel/include/display_state.h
@@ -4,6 +4,8 @@
 
 DisplayModel reduce_status(const HubStatus& status, bool wifi_connected,
                            bool hub_connected);
+std::string connection_diagnostic(bool wifi_connected, bool hub_connected,
+                                  int http_status);
 DisplayAction next_action(const DisplayModel& model);
 DisplayInteraction primary_press(DisplayPage page, const DisplayModel& model);
 DisplayPage move_page(DisplayPage current, bool forward);
